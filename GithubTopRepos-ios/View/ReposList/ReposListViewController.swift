e8d4775092a8ac978beb4b59c82504765deeaea7
diegogbf <diego.gbfernandes@gmail.com>
2020-03-30 23:58:42 -0300

    setting up pagination

--- GithubTopRepos-ios/View/ReposList/ReposListViewController.swift
+++ GithubTopRepos-ios/View/ReposList/ReposListViewController.swift
@@ -14,14 +14,18 @@ import RxCocoa
 class ReposListViewController: UIViewController {
     
     private lazy var segmentControl: UISegmentedControl = {
-        let segmentControl = UISegmentedControl(items: [RepoListOrderType.stars.rawValue, RepoListOrderType.followers.rawValue])
+        let segmentControl = UISegmentedControl(items: [RepoListOrderType.stars.rawValue, RepoListOrderType.watchers.rawValue])
         segmentControl.backgroundColor = .white
+        segmentControl.selectedSegmentIndex = 0
         return segmentControl
     }()
     
     private lazy var tableView: CustomTableView = {
         let tableView = CustomTableView(frame: .zero)
-        tableView.register(RepoTableViewCell.self, forCellReuseIdentifier: "RepoTableViewCell")
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(RepoTableViewCell.self)
+        tableView.register(LoaderCell.self)
         return tableView
     }()
     
@@ -31,8 +35,9 @@ class ReposListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
-        fetchData()
         setupBiding()
+        viewModel.delegate = self
+        viewModel.filterType.accept(.stars)
     }
     
     private func setup() {
@@ -45,7 +50,9 @@ class ReposListViewController: UIViewController {
         }
         
         tableView.pullToRefreshAction = { [weak self] in
-            self?.fetchData()
+            self?.viewModel.resetPagination()
+            self?.tableView.reloadData()
+            self?.viewModel.fetchRepos()
         }
     }
     
@@ -53,25 +60,62 @@ class ReposListViewController: UIViewController {
         segmentControl
             .rx
             .selectedSegmentIndex
-            .map{ $0 == 0 ? RepoListOrderType.stars : RepoListOrderType.followers }
+            .map{ $0 == 0 ? RepoListOrderType.stars : RepoListOrderType.watchers }
             .bind(to: viewModel.filterType)
             .disposed(by: disposeBag)
-        
-        viewModel
-            .repositories
-            .bind(to: tableView.rx.items(cellIdentifier: "RepoTableViewCell", cellType: RepoTableViewCell.self)) { row, element, cell in
-                cell.setup(repository: element)
-        }.disposed(by: disposeBag)
-    }
-    
-    private func fetchData() {
-        tableView.showLoader(true)
-        viewModel.fetchRepos(onSuccess: { [weak self] in
-            DispatchQueue.main.async {
-                self?.tableView.showLoader(false)
-                self?.tableView.reloadData()
-            }
-        }) { _ in  }
     }
 }
 
+extension ReposListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return section == 0 ? viewModel.repositories.count : 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: RepoTableViewCell.reuseId, for: indexPath) as! RepoTableViewCell
+            cell.setup(repository: viewModel.repositories[indexPath.row])
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: LoaderCell.reuseId, for: indexPath) as! LoaderCell
+            cell.indicator.startAnimating()
+            return cell
+        }
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return viewModel.isPaginating ? 2 : 1
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let repo = viewModel.repositories[indexPath.row]
+        if let link = URL(string: repo.htmlURL ?? "") {
+          UIApplication.shared.open(link)
+        }
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if scrollView.contentOffset.y > (scrollView.contentSize.height - scrollView.frame.height) {
+            if !viewModel.repositories.isEmpty && viewModel.shouldPaginate && !viewModel.isPaginating {
+                viewModel.isPaginating = true
+                viewModel.fetchRepos()
+            }
+        }
+    }
+}
+
+extension ReposListViewController: RepoListDelegate {
+    func showError(message: String) {
+        showErrorAlert(msg: message)
+    }
+    
+    func reloadData() {
+        tableView.reloadData()
+    }
+    
+    func showLoader(_ show: Bool) {
+        if !viewModel.isPaginating {
+            tableView.showLoader(show)
+        }
+    }
+}
--- GithubTopRepos-ios/View/ReposList/ResposListViewModel.swift
+++ GithubTopRepos-ios/View/ReposList/ResposListViewModel.swift
@@ -12,32 +12,70 @@ import RxCocoa
 
 enum RepoListOrderType: String {
     case stars
-    case followers
-    
-    var filterTag: String {
-        switch self {
-        case .stars:
-            return "stars"
-        case .followers:
-            return "watchers"
-        }
-    }
+    case watchers
+}
+
+protocol RepoListDelegate: class {
+    func showError(message: String)
+    func reloadData()
+    func showLoader(_ show: Bool)
 }
 
 class ReposListViewModel {
     // MARK: - Variables
-    var repositories = BehaviorRelay<[Repository]>(value: [])
+    var repositories = [Repository]()
     var currentPage: Int = 0
     var filterType = BehaviorRelay<RepoListOrderType>(value: .stars)
+    var loading = PublishSubject<Bool>()
+    let bag = DisposeBag()
+    var isPaginating = false {
+        didSet {
+            self.delegate?.reloadData()
+        }
+    }
+    var shouldPaginate = true
+    
+    weak var delegate: RepoListDelegate?
+    
+    init() {
+        setupObservables()
+    }
+    
+    func resetPagination() {
+        currentPage = 0
+        shouldPaginate = true
+        isPaginating = false
+        repositories.removeAll()
+    }
+    
+    private func setupObservables() {
+        filterType
+            .asObservable()
+            .subscribe(onNext: { [weak self] _ in
+                self?.resetPagination()
+                self?.delegate?.reloadData()
+                self?.fetchRepos()
+            }
+        ).disposed(by: bag)
+    }
     
     // MARK: - Api Request
-    func fetchRepos(onSuccess: @escaping ()->(), onError: @escaping(String)->()) {
-        let request = ReposListHeader(sort: filterType.value.filterTag, page: currentPage)
-        ServiceLayer.request(route: Router.listRepos(request: request), onSuccess: { (response: RepositoriesList) in
-            self.repositories.accept(response.items ?? [])
-            onSuccess()
-        }) { (msg) in
-            onError(msg)
+    func fetchRepos() {
+        if shouldPaginate {
+            let request = ReposListHeader(sort: filterType.value.rawValue, page: currentPage)
+            
+            delegate?.showLoader(true)
+            ServiceLayer.request(route: Router.listRepos(request: request), onSuccess: { (response: RepositoriesList) in
+                self.delegate?.showLoader(false)
+                self.repositories.append(contentsOf: response.items ?? [])
+                self.shouldPaginate = (response.items?.count ?? 0) != 0
+                self.currentPage += 1
+                self.isPaginating = false
+            }) { (msg) in
+                self.delegate?.showLoader(false)
+                self.isPaginating = false
+                self.delegate?.showError(message: msg)
+            }
         }
     }
 }
